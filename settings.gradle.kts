27d774d30e1bc223bcf3a3fcc05c2758f64655df
reocat <111279389+reocat@users.noreply.github.com>
2024-10-04 19:47:26 +0300

    settings: Add Xposed API to sources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-		maven(url = "https://jcenter.bintray.com")
+        maven(url = "https://jcenter.bintray.com")
+        maven(url = "https://api.xposed.info/")
     }
 }
 
